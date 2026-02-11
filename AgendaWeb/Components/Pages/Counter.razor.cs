9fddd3e8483de7dbe0d7c0ea981e5c3b535c7ae8
Majo-2306 <maria_castro24@unikino.edu.mx>
2026-02-11 07:58:41 -0700

    Agregar archivos de proyecto.

---
+++ AgendaWeb/Components/Pages/Counter.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.SqlTypes;
+
+namespace AgendaWeb.Components.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+
+            string connectionString = "Server=localhost;Database=AgendaDb;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = "INSERT into Contactos(Nombre, Telefono, Email) values('Desde VS', '6221194659', 'aylins2323@gmail.com')";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            cmd.CommandType= CommandType.Text;
+
+            connection.Open();
+
+            cmd.ExecuteNonQuery();
+
+            connection.Close();
+
+            connection.Dispose();
+        }
+    }
+}
